2d4db6a91f837a46f39f2f316e1ba1ba17f8b3d6
farciomernandes <farciomernandes@gmail.com>
2021-04-27 13:50:56 -0300

    add containers for controller render

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,27 +5,28 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 
 import { selectCollection } from '../../redux/shop/shop.selector';
 
-import './collection.styles.scss';
+import {
+  CollectionPageContainer,
+  CollectionTitle,
+  CollectionItemsContainer
+} from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
+    <CollectionPageContainer>
+      <CollectionTitle>{title}</CollectionTitle>
+      <CollectionItemsContainer>
         {items.map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </CollectionItemsContainer>
+    </CollectionPageContainer>
   );
 };
 
 const mapStateToProps = (state, ownProps) => ({
-    /**This is necessary because unlike others selectors, 
-     * this selector needs a part of the state depending on the URL parameter
-     */
-    collection: selectCollection(ownProps.match.params.collectionId)(state)
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
 });
 
 export default connect(mapStateToProps)(CollectionPage);
